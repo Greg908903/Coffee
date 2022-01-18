c4f702bcd2e680aa59283ff8b048b28bfb9464b9
Гриша <grisha.novgorodtsev@gmail.com>
2022-01-18 17:57:05 +0300

    First code

---
+++ main.py
@@ -0,0 +1,38 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.select_data()
+
+    def select_data(self):
+        res = self.connection.cursor().execute("SELECT * FROM Sorts").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(['ID', 'Название сорта', 'Степень обжарки (до 10)',
+                                                    'В зернах (да или нет)', 'Описание вкуса',
+                                                    'Стоимость', 'Масса в упаковке (в кг)'])
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
